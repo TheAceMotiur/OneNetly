a8cdd7b45f0e60bcd8382e6130424f48759ef288
OneNetly <admin@onenetly.com>
2024-10-12 15:10:57 +0300

    ok

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php  
+	define("DB_NAME", 'onenetly_onenetly');
+	define("DB_USER", 'onenetly_onenetly');
+	define("DB_PASSWORD", 'AmiMotiur27@');
+	define("DB_HOST", 'localhost');
+	define("DB_PORT", '3306');
+	define("SYS_URL", 'https://onenetly.com');
+	define("DEBUGGING", false);
+	define("DEFAULT_LOCALE", 'en_us');
+	define("LICENCE_KEY", '6gjMWLZsP-4Lnkm-4MDqB-4Rjfl-4MNCK-6bd855c2f1d7');
+  define("JWT_SECRET", 'e553192e1cf4f18165de6be1752f60af');
+	?>
